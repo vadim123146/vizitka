0be6d3c202104aa799e62457019834c34bacc1eb
vadim123146 <lukancevvadim079@gmail.com~>
2025-02-12 23:20:38 +0300

    2-2320

---
+++ js/blogs.js
@@ -0,0 +1,15 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+   const accordionHeaders = document.querySelectorAll('.accordion-header');
+
+   accordionHeaders.forEach(header => {
+      header.addEventListener('click', function() {
+         const content = this.nextElementSibling;
+         if (content.style.display === 'block') {
+            content.style.display = 'none';
+         } else {
+            content.style.display = 'block';
+         }
+      });
+   });
+});
